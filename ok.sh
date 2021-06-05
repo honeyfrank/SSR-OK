8cf9e29036e3b1d007803e5e5353854fa8ed5c01
Frank <188005954@qq.com>
2021-06-06 03:35:19 +0800

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,11 @@
+#启用BBR+fq
+startbbr(){
+	
+	echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	sysctl -p
+	echo "BBR启动"
+  lsmod | grep bbr
+
+  
+  
