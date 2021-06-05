e9b8d254fa48ac8aa36f7ffd83a7f775008e8204
Frank <188005954@qq.com>
2021-06-06 06:25:28 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -8,6 +8,17 @@ startbbr() {
 	echo "BBR启动"
   
 }
+
+
+
+#安装nginx
+menu() {
+   
+   sudo apt update
+    sudo apt install nginx
+
+   
+}
   
 #菜单 
 menu() {
@@ -15,6 +26,9 @@ menu() {
 	net_qdisc=`cat /proc/sys/net/core/default_qdisc | awk '{print $1}'`
 	echo "1.启动BBR"
 	echo " 当前拥塞控制算法为: ${net_congestion_control} 当前队列算法为: ${net_qdisc} "
+	echo "2.安装shadowsocksR"
+	echo "3.安装nginx伪装网站"
+	echo "4.卸载nginx"
 	
 	
 	read -p " 请输入数字 :" num
