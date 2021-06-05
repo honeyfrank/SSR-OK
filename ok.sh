ed1dc0c71b6a0952dda3bd940061e19fcce7738e
Frank <188005954@qq.com>
2021-06-06 06:29:49 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -24,12 +24,17 @@ menu() {
 menu() {
 	net_congestion_control=`cat /proc/sys/net/ipv4/tcp_congestion_control | awk '{print $1}'`
 	net_qdisc=`cat /proc/sys/net/core/default_qdisc | awk '{print $1}'`
+	echo "=============================================================="
 	echo "1.启动BBR"
-	echo " 当前拥塞控制算法为: ${net_congestion_control} 当前队列算法为: ${net_qdisc} "
+	echo "当前拥塞控制算法为: ${net_congestion_control} 当前队列算法为: ${net_qdisc} "
+	echo ""
 	echo "2.安装shadowsocksR"
-	echo "3.安装nginx伪装网站"
+	echo ""
+	echo "3.安装nginx伪装网站"	
+	echo ""
 	echo "4.卸载nginx"
-	
+	echo "=============================================================="
+
 	
 	read -p " 请输入数字 :" num
         case "$num" in
